57e5221ecea04589cd5a8ca5a8cf9025d2dd9fda
dc19679 <58332500+dc19679@users.noreply.github.com>
2023-08-18 19:53:25 +0100

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -7,6 +7,16 @@ Users can also input a text file to encrypt, decrypt or brute force.
 """
 
 def encrypt(message, rotational_value):
+    """
+    Function that encrypts the user's messages based on their input and the
+    rotational value.
+
+    :param message: The message input from the user
+    :param rotational_value: An integer that determines how much each letter
+                             is shifted to obtain the corresponding encrypted
+                             or decrypted letter.
+    :return: Encrypted message
+    """
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     encrypted = ""
 
@@ -21,15 +31,37 @@ def encrypt(message, rotational_value):
     return encrypted
 
 def decrypt(message, rotational_value):
+    """
+    Function that decrypts the user's message based on their input message
+    and the rotational value. The decryption works nearly identically as
+    the encryption, expect the rotational value is negative.
+
+    :param message: The message input from the user
+    :param rotational_value: An integer that determines how much each letter
+                             is shifted to obtain the corresponding encrypted
+                             or decrypted letter.
+    :return: Decrypted message
+    """
     return encrypt(message, -rotational_value)
 
 def brute_force_decrypt(message):
+    """
+    Function that peforms brute attack force decrypt a message encrypted using the
+    Caesar cipher.
+
+    :param message: The encrypted message to be decrypted from the user
+    :return: A list containing decrypted messages for all possible rotational values
+    """
     decrypted_messages = []
     for rv in range(26):
         decrypted_messages.append(decrypt(message, rv))
     return decrypted_messages
 
 def main():
+    """
+    Function performs Caesar cipher operations based on user input.
+    :return: None
+    """
     print("Would you like to decrypt, encrypt, or brute force your message?")
     mode = input().lower()
 
@@ -56,6 +88,11 @@ def main():
         print("Invalid mode choice.")
 
 def get_message_input():
+    """
+    Function that gets the input message from the user
+
+    :return: The input message as a string
+    """
     file_option = input("Would you like to open a file? (YES/NO): ").strip().lower()
 
     if file_option == "yes":
@@ -68,6 +105,11 @@ def get_message_input():
     return message
 
 def get_rotational_value():
+    """
+    Function that gets the rotational value from the user
+
+    :return: The rotational value as an integer
+    """
     rotational_value = int(input("Enter a positive integer for rotational value: "))
     return rotational_value
 
